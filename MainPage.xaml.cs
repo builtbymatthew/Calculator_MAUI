6f7b6444484bca405a1e3e6d0082e15f65fd1aa7
Matthew Pearson <matt11pearson@gmail.com>
2023-09-20 18:04:05 -0700

    commented all the code

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿
 using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel;
+using System.Collections.Generic;
 
 
 namespace CaclulatorMAUI;
@@ -9,24 +12,24 @@ namespace CaclulatorMAUI;
 
 class Calculator
 {
-    public static double GetNum(string numText)
+    public static double GetNum(string numText) //function definition for Getnum
     {
-        double numTemp = 0;
+        double numTemp = 0; //temporary value to store the outputted double
 
-        if (numText != null) //if textbox is = to 0
+        if (numText != null) //if textbox is = 0
         {
-            if (double.TryParse(numText, out numTemp))
+            if (double.TryParse(numText, out numTemp)) //check if input is a valid double by converting the string to a double
             {
-                return numTemp;//check if input is a valid double 
+                return numTemp;//if it converts to a double, return the value
             }
             else
             {
-                return 0;
+                return 0;//if it fails, return 0
             }
         }
         else
         {
-            return 0;
+            return 0; //if the textbox is = 0, return 0
         }
     }
 }
@@ -34,32 +37,33 @@ class Calculator
 
 
 
-public partial class MainPage : ContentPage
+public partial class MainPage : ContentPage //create a new class to store code for our apps main page
     {
-        int count = 0;
+        int count = 0; //new global variable call count
 
-        public MainPage()
+        public MainPage() //new function called main paige
         {
-            InitializeComponent();
+            InitializeComponent(); //initializes our main page
         }
 
-        public string correctText1;
+        //new gloabal global strings to store 
+        public string correctText1; 
         public string correctText2;
 
-        private void entryNum1_TextChanged(object sender, TextChangedEventArgs e)
+        private void entryNum1_TextChanged(object sender, TextChangedEventArgs e) //new function for user entry 1
         {
-            double numTemp1 = 0;
-            if(double.TryParse(entryNum1.Text, out numTemp1))
+            double numTemp1 = 0; //new temp double
+            if(double.TryParse(entryNum1.Text, out numTemp1)) //try and convert string inputted by user into a double
         {
-            correctText1 = entryNum1.Text;
+            correctText1 = entryNum1.Text; //store user input 1 in correct text string
         }
-        else
+        else //otherwise
         {
-            entryNum1.CursorPosition = correctText1.Length;
-            int selectionLength = e.NewTextValue.Length - correctText1.Length;
-            if (selectionLength > 0)
+            entryNum1.CursorPosition = correctText1.Length;//try and convert string inputted by user into a double, if successful, continue
+            int selectionLength = e.NewTextValue.Length - correctText1.Length;//highlight all the characters that are invalid
+            if (selectionLength > 0)//do not proceed until all invalid characters have been earsed
             {
-                entryNum1.SelectionLength = selectionLength;
+                entryNum1.SelectionLength = selectionLength;//keep checking to see if invalid characters have been erased 
             }
         }
 
@@ -68,94 +72,94 @@ public partial class MainPage : ContentPage
         private void entryNum2_TextChanged(object sender, TextChangedEventArgs e)
         {
             double numTemp2 = 0;
-            if (double.TryParse(entryNum2.Text, out numTemp2))
+            if (double.TryParse(entryNum2.Text, out numTemp2)) //try and convert string inputted by user into a double, if successful, continue
+        {
+                correctText2 = entryNum1.Text; //store user input 2 in correct text string
+        }
+            else //otherwise
             {
-                correctText2 = entryNum1.Text;
-            }
-            else
-            {
-                entryNum2.CursorPosition = correctText2.Length;
-                int selectionLength = e.NewTextValue.Length - correctText2.Length;
-                if (selectionLength > 0)
+                entryNum2.CursorPosition = correctText2.Length; //get the number of characters in user input 1, and set the position of the cursor
+                int selectionLength = e.NewTextValue.Length - correctText2.Length; //highlight all the characters that are invalid
+                if (selectionLength > 0) //do not proceed until all invalid characters have been earsed
                 {
-                    entryNum2.SelectionLength = selectionLength;
+                    entryNum2.SelectionLength = selectionLength; //keep checking to see if invalid characters have been erased 
                 }
             }
         }
 
-        private void btnAdd_Clicked(object sender, EventArgs e)
+        private void btnAdd_Clicked(object sender, EventArgs e) //if the 'add' button is clicked, call this function
         {
             //double num1 = Convert.ToDouble(entryNum1.Text); //didnt work for 123a and backspaces, i.e. got errors
             //double num2 = Convert.ToDouble(entryNum2.Text);
 
-            double aNum1 = Calculator.GetNum(entryNum1.Text);
-            double aNum2 = Calculator.GetNum(entryNum2.Text);    
+            double aNum1 = Calculator.GetNum(entryNum1.Text);//double to store first number inputted by the user
+            double aNum2 = Calculator.GetNum(entryNum2.Text); //double to store second number inputted by the user   
 
-            double ans = aNum1 + aNum2; //"Add"num1 + "Add"num2
-            txtAnswer.Text = Convert.ToString(ans);
-        }
-
-        private void btnSubtract_Clicked(object sender, EventArgs e)
-        {
-             double sNum1 = Calculator.GetNum(entryNum1.Text);
-             double sNum2 = Calculator.GetNum(entryNum2.Text);
-
-             double ans = sNum1 - sNum2;
-             txtAnswer.Text = Convert.ToString(ans);
+            double ans = aNum1 + aNum2; //"Add"num1 + "Add"num2, store in a new double called answer
+            txtAnswer.Text = Convert.ToString(ans); //convert double 'answer' to string and write it as the answer in the GUI
     }
 
-        private void btnMultiply_Clicked(object sender, EventArgs e)
-        {
-            double mNum1 = Calculator.GetNum(entryNum1.Text);
-            double mNum2 = Calculator.GetNum(entryNum2.Text);
-
-            double ans = mNum1 * mNum2;
-            txtAnswer.Text = Convert.ToString(ans);
-    }
-
-        private void btnDivide_Clicked(object sender, EventArgs e)
-        {
-            double dNum1 = Calculator.GetNum(entryNum1.Text);
-            double dNum2 = Calculator.GetNum(entryNum2.Text);
-
-            double ans = dNum1 / dNum2;
-            txtAnswer.Text = Convert.ToString(ans);
-    }
-
-    private void btnSquare_Clicked(object sender, EventArgs e)
+        private void btnSubtract_Clicked(object sender, EventArgs e)//if the 'subtract' button is clicked, call this function
     {
-        double sqrNum1 = Calculator.GetNum(entryNum1.Text);
-        double sqrNum2 = Calculator.GetNum(entryNum2.Text); 
+             double sNum1 = Calculator.GetNum(entryNum1.Text);// double to store first number inputted by the user
+             double sNum2 = Calculator.GetNum(entryNum2.Text);//double to store second number inputted by the user
 
-        double ans = System.Math.Pow(sqrNum1, sqrNum2);
-        txtAnswer.Text = Convert.ToString(ans); 
+        double ans = sNum1 - sNum2; //num1 - num2, store in a new double called answer
+        txtAnswer.Text = Convert.ToString(ans); //convert double 'answer' to string and write it as the answer in the GUI
     }
 
-    private void btnSqrt_Clicked(object sender, EventArgs e)
+        private void btnMultiply_Clicked(object sender, EventArgs e)//if the 'multiply' button is clicked, call this function
     {
-        double sqrtNum1 = Calculator.GetNum(entryNum1.Text);
-        double sqrtNum2 = Calculator.GetNum(entryNum2.Text);
+            double mNum1 = Calculator.GetNum(entryNum1.Text);// double to store first number inputted by the user
+            double mNum2 = Calculator.GetNum(entryNum2.Text);//double to store second number inputted by the user
 
-        double ans = System.Math.Sqrt(sqrtNum1);
-        txtAnswer.Text = Convert.ToString(ans);
+            double ans = mNum1 * mNum2; //num1 * num2, store in a new double called answer
+            txtAnswer.Text = Convert.ToString(ans);//convert double 'answer' to string and write it as the answer in the GUI
     }
 
-    private void btnExp_Clicked(object sender, EventArgs e)
+        private void btnDivide_Clicked(object sender, EventArgs e)//if the 'divide' button is clicked, call this function
     {
-        double expNum1 = Calculator.GetNum(entryNum1.Text);
-        double expNum2 = Calculator.GetNum(entryNum2.Text);
+            double dNum1 = Calculator.GetNum(entryNum1.Text);// double to store first number inputted by the user
+            double dNum2 = Calculator.GetNum(entryNum2.Text);//double to store second number inputted by the user
 
-        double ans = System.Math.Exp(expNum1);
-        txtAnswer.Text = Convert.ToString(ans);
+            double ans = dNum1 / dNum2; //num1 / num2, store in a new double called answer
+            txtAnswer.Text = Convert.ToString(ans); //convert double 'answer' to string and write it as the answer in the GUI
     }
 
-    private void btnCbrt_Clicked(object sender, EventArgs e)
+    private void btnSquare_Clicked(object sender, EventArgs e)//if the 'square' button is clicked, call this function
     {
-        double cbrtNum1 = Calculator.GetNum(entryNum1.Text);
-        double cbrtNum2 = Calculator.GetNum(entryNum2.Text);
+        double sqrNum1 = Calculator.GetNum(entryNum1.Text);// double to store first number inputted by the user
+        double sqrNum2 = Calculator.GetNum(entryNum2.Text); //double to store second number inputted by the user
 
-        double ans = System.Math.Cbrt(cbrtNum1);
-        txtAnswer.Text = Convert.ToString(ans);
+        double ans = System.Math.Pow(sqrNum1, sqrNum2); //num1 ^ (raised to the power of) num2, store in a new double called answer
+        txtAnswer.Text = Convert.ToString(ans); //convert double 'answer' to string and write it as the answer in the GUI
+    }
+
+    private void btnSqrt_Clicked(object sender, EventArgs e)//if the 'square root' button is clicked, call this function
+    {
+        double sqrtNum1 = Calculator.GetNum(entryNum1.Text);// double to store first number inputted by the user
+        double sqrtNum2 = Calculator.GetNum(entryNum2.Text);//double to store second number inputted by the user
+
+        double ans = System.Math.Sqrt(sqrtNum1);//sqrt of num1, store in a new double called answer
+        txtAnswer.Text = Convert.ToString(ans); //convert double 'answer' to string and write it as the answer in the GUI
+    }
+
+    private void btnExp_Clicked(object sender, EventArgs e)//if the 'exponent' button is clicked, call this function
+    {
+        double expNum1 = Calculator.GetNum(entryNum1.Text);// double to store first number inputted by the user
+        double expNum2 = Calculator.GetNum(entryNum2.Text);//double to store second number inputted by the user
+
+        double ans = System.Math.Exp(expNum1);//e ^ of num1, store in a new double called answer
+        txtAnswer.Text = Convert.ToString(ans);//convert double 'answer' to string and write it as the answer in the GUI
+    }
+
+    private void btnCbrt_Clicked(object sender, EventArgs e)//if the 'cube root' button is clicked, call this function
+    {
+        double cbrtNum1 = Calculator.GetNum(entryNum1.Text);// double to store first number inputted by the user
+        double cbrtNum2 = Calculator.GetNum(entryNum2.Text); //double to store second number inputted by the user
+
+        double ans = System.Math.Cbrt(cbrtNum1);//cbrt of num1, store in a new double called answer
+        txtAnswer.Text = Convert.ToString(ans); //convert double 'answer' to string and write it as the answer in the GUI
     }
 
   
@@ -163,40 +167,40 @@ public partial class MainPage : ContentPage
 
     
 
-    private void btnRndm_Clicked(object sender, EventArgs e)
+    private void btnRndm_Clicked(object sender, EventArgs e)//if the 'random' button is clicked, call this function
     {
-        double randNum1 = Calculator.GetNum(entryNum1.Text);
-        double randNum2 = Calculator.GetNum(entryNum2.Text);
+        double randNum1 = Calculator.GetNum(entryNum1.Text);// double to store first number inputted by the user
+        double randNum2 = Calculator.GetNum(entryNum2.Text); //double to store second number inputted by the user
 
         
         
-        Random rand = new Random();
+        Random rand = new Random(); //store new instance of random class in a Random variable called rand
        
 
-        if(randNum1 < randNum2)
+        if(randNum1 <= randNum2) //if 1st number inputted is greater than the second number inputted
         {
-            double answer = rand.Next((int)randNum1, (int)randNum2);
+            double answer = rand.Next((int)randNum1, (int)randNum2); //return a random number between 1st number inputted and second number inputted and store it in a new double called result
 
-            if(answer == 12)
+            if(answer == 12) //if the answer is = 12
             {
-                txtAnswer.Text = Convert.ToString("Your number is 12, and my name is Matt Pearson");
+                txtAnswer.Text = Convert.ToString("Your number is 12, and my name is Matt Pearson"); // write the random number and my name as the answer
             }
-            else
+            else //if not = 12
             {
-                txtAnswer.Text = Convert.ToString(answer);
+                txtAnswer.Text = Convert.ToString(answer); //convert double 'answer' to string and write it as the answer in the GUI
             }
         
         } 
-        else
+        else //if num2 is less than or = num1
         {
-            double answer = rand.Next((int)randNum2, (int)randNum1);
-            if(answer == 12)
+            double answer = rand.Next((int)randNum2, (int)randNum1); //return a random number between 2nd number inputted and 1st number inputted and store it in a new double called answer
+            if (answer == 12) //if the random number is 12, write my name as the answer
             {
-                txtAnswer.Text = Convert.ToString("Your number is 12, and my name is Matt Pearson");
+                txtAnswer.Text = Convert.ToString("Your number is 12, and my name is Matt Pearson"); //write the random number and my name as the answer
             }
-            else
+            else //if not = 12
             {
-                txtAnswer.Text = Convert.ToString(answer);
+                txtAnswer.Text = Convert.ToString(answer); //convert double 'answer' to string and write it as the answer in the GUI
             }
         }
         
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -3,24 +3,25 @@
 namespace CaclulatorMAUI
 {
 
-    public static class MauiProgram
+    public static class MauiProgram //new class for MauiProgram
     {
-        public static MauiApp CreateMauiApp()
+        public static MauiApp CreateMauiApp() //initialize a new function call CreateMauiApp
         {
-            var builder = MauiApp.CreateBuilder();
+            var builder = MauiApp.CreateBuilder(); //initializes a new instance of MauiAppBuilder. 
             builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
+                .UseMauiApp<App>() //use the MauiApp extenstion for the framwork of our GUI
+                .ConfigureFonts(fonts => //use the configure fonts function to include other types of fonts for our GUI
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    //add these two fonts to our font library
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular"); 
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+		builder.Logging.AddDebug(); //create a debug logger named debug
 #endif
 
-            return builder.Build();
+            return builder.Build(); //build the MauiApp
         }
     }
 }
